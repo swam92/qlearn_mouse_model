e00d3b2253f444110f50e8c20a2ed1e5c2126610
swam92 <svw223@nyu.edu>
2015-05-03 22:14:29 -0400

    initial commit

---
+++ driver.py
@@ -0,0 +1,51 @@
+count = 0
+alpha = .1
+for x in range(0, 110):
+	if x >= 0 and x <10:
+		print "alpha = .1 gamma = 0"
+	if x >= 10 and x < 20:
+		print "alpha = .1 gamma = .1"
+	if x >= 20 and x < 30:
+		print "alpha = .1 gamma = .2"
+	if x >= 30 and x < 40:
+		print "alpha = .1 gamma = .3"
+	if x >= 40 and x < 50:
+		print "alpha = .1 gamma = .4"
+	if x >= 50 and x < 60:
+		print "alpha = .1 gamma = .5"
+	if x >= 60 and x < 70:
+		print "alpha = .1 gamma = .6"
+	if x >= 70 and x < 80:
+		print "alpha = .1 gamma = .7"
+	if x >= 80 and x < 90:
+		print "alpha = .1 gamma = .8"
+	if x >= 90 and x < 100:
+		print "alpha = .1 gamma = .9"
+	if x >= 100 and x < 110:
+		print "alpha = .1 gamma = 1"
+
+gamma = .9
+
+for x in range(0, 110):
+	if x >= 0 and x <10:
+		print "alpha = .1 gamma = 0"
+	if x >= 10 and x < 20:
+		print "alpha = .1 gamma = .1"
+	if x >= 20 and x < 30:
+		print "alpha = .1 gamma = .2"
+	if x >= 30 and x < 40:
+		print "alpha = .1 gamma = .3"
+	if x >= 40 and x < 50:
+		print "alpha = .1 gamma = .4"
+	if x >= 50 and x < 60:
+		print "alpha = .1 gamma = .5"
+	if x >= 60 and x < 70:
+		print "alpha = .1 gamma = .6"
+	if x >= 70 and x < 80:
+		print "alpha = .1 gamma = .7"
+	if x >= 80 and x < 90:
+		print "alpha = .1 gamma = .8"
+	if x >= 90 and x < 100:
+		print "alpha = .1 gamma = .9"
+	if x >= 100 and x < 110:
+		print "alpha = .1 gamma = 1"
