91ba875930400cdb538334776b33f8ed32902ce6
Stephen Spivack <stephenspivack@Stephens-MacBook-Air.local>
2015-05-03 22:53:44 -0400

    added function that calls the driver file

--- driver.py
+++ driver.py
@@ -8,90 +8,70 @@ alpha = .1
 
 for x in range(0, 110):
 	if x >= 0 and x <10:
-		print "alpha = .1 gamma = 0"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, 0)
+	
 	if x >= 10 and x < 20:
-		print "alpha = .1 gamma = .1"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .1)
+	
 	if x >= 20 and x < 30:
-		print "alpha = .1 gamma = .2"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .2)
 
 	if x >= 30 and x < 40:
-		print "alpha = .1 gamma = .3"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .3)
 
 	if x >= 40 and x < 50:
-		print "alpha = .1 gamma = .4"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .4)
 
 	if x >= 50 and x < 60:
-		print "alpha = .1 gamma = .5"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .5)
 
 	if x >= 60 and x < 70:
-		print "alpha = .1 gamma = .6"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .6)
 
 	if x >= 70 and x < 80:
-		print "alpha = .1 gamma = .7"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .7)
 
 	if x >= 80 and x < 90:
-		print "alpha = .1 gamma = .8"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .8)
 
 	if x >= 90 and x < 100:
-		print "alpha = .1 gamma = .9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .9)
 
 	if x >= 100 and x < 110:
-		print "alpha = .1 gamma = 1"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, 1)
 
 
 gamma = .9
 for x in range(0, 110):
 	if x >= 0 and x <10:
-		print "alpha = 0 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, 0, .9)
 
 	if x >= 10 and x < 20:
-		print "alpha = .1 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .1, .9)
 
 	if x >= 20 and x < 30:
-		print "alpha = .2 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .2, .9)
 
 	if x >= 30 and x < 40:
-		print "alpha = .3 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .3, .9)
 
 	if x >= 40 and x < 50:
-		print "alpha = .4 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .4, .9)
 
 	if x >= 50 and x < 60:
-		print "alpha = .5 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .5, .9)
 
 	if x >= 60 and x < 70:
-		print "alpha = .6 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .6, .9)
 
 	if x >= 70 and x < 80:
-		print "alpha = .7 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .7, .9)
 
 	if x >= 80 and x < 90:
-		print "alpha = .8 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .8, .9)
 
 	if x >= 90 and x < 100:
-		print "alpha = .9 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, .9, .9)
 
 	if x >= 100 and x < 110:
-		print "alpha = 1 gamma = 0.9"
-		cliff_Q.main(arg1)
+		cliff_Q.main(.1, 1, .9)
