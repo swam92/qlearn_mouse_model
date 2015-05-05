c2f24c7c308d61b08d7bfe6c2dd2ce2080121b94
swam92 <svw223@nyu.edu>
2015-05-04 22:46:49 -0400

    adding rl

---
+++ rl.py
@@ -0,0 +1,53 @@
+"""
+Author: Stephen Spivack
+Created: Fri May 4 15:09:02 2015
+
+"""
+import cellular
+import qlearn
+import time
+import sys
+import cliff_Q
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+
+alphas = np.linspace(0, 1, 11) 
+gammas = np.linspace(0, 1, 11)
+
+ntrials = 100
+
+success = np.zeros((len(alphas), len(gammas)))
+#print(success)
+
+def simulate(alpha, gamma):
+	x = 0
+	x = cliff_Q.begin(.1, alpha, gamma)
+	sys.exit()
+	return x
+
+for i in xrange(len(alphas)):
+    for j in xrange(len(gammas)):
+        alpha = alphas[i]
+        gamma = gammas[j]        
+        
+        print("alpha = {}, gamma = {}".format(alpha, gamma))    
+        success[i,j] = simulate(alpha, gamma)
+
+np.savetxt('data.txt', success)
+
+#######################retrieve data and plot it#########################
+
+#success = np.loadtxt('data.txt')
+
+#plt.figure()
+
+#plt.imshow(success)
+        
+#plt.figure()
+#ax = plt.gca(projection='3d')
+
+#X, Y = np.meshgrid(alphas, gammas)
+
+#ax.plot_surface(X, Y, success, cmap=plt.cm.coolwarm)
